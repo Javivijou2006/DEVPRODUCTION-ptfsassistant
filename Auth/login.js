2074e2b76922ef70555e654370730a6744980317
Javivijou2006 <67285545+Javivijou2006@users.noreply.github.com>
2021-01-03 10:28:59 +0000

    Add files via upload

--- Auth/login.js
+++ Auth/login.js
@@ -1,30 +1,87 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: 'AIzaSyA9PSOmJuJIh7k_ZBl6HjVyCpuWv9QLZwE',
-  authDomain: 'test-4189f.firebaseapp.com',
-  projectId: 'test-4189f'
-});
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+  var config = {
+    apiKey: "AIzaSyA9PSOmJuJIh7k_ZBl6HjVyCpuWv9QLZwE",
+    authDomain: "test-4189f.firebaseapp.com",
+    databaseURL: "https://.firebaseio.com",
+    storageBucket: "test-4189f.appspot.com"
+  };
+  firebase.initializeApp(config);
 
-var db = firebase.firestore();
+  // Get a reference to the database service
+  var database = firebase.database();
+  
+  function createacc() {
+    var email = document.getElementById('SignEmail').value
+    var password = document.getElementById('SignPassword').value
+    var username = document.getElementById('username').value
+    //create the account on Auth
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((user) => {
+    
+    //create database
+    var email = document.getElementById('SignEmail')
+    var ID2 = email.toString().replace('@', '')
+   var ID = email.toString().replace('.', '')
+  firebase.database().ref('users/'+ID).set({
+    username: username
+  });
+  var message = document.getElementById('createAccMessage')
+    message.innerHTML = " your account have been created, wait until we create all the databases and log in into your account!"
+    message.style.opacity = 1;
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+      var message = document.getElementById('createAccMessage')
+    message.innerHTML = " something went wrong, "+errorMessage
+    message.style.opacity = 1;
+     message.style.backgroundColor = "#EF8787";
+    message.style.color = "#EEEEEE";
+  });
 
+  }
+
+function createDatabase(username) {
+  var email = document.getElementById('SignPassword');
+  var ID1 = email.replace("@", "")
+  var ID = ID2.replace(".", "")
+  firebase.database().ref('users/'+ID).set({
+    username: username
+  });
+    var message = document.getElementById('logInMessage')
+    message.innerHTML = " user authenticated, loggin in"
+    message.style.opacity = 1;
+}
 
 function login() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
+  
   firebase.auth().signInWithEmailAndPassword(email, password)
   .then((user) => {
-    // Signed in 
+      var message = document.getElementById('logInMessage')
+    message.innerHTML = " user authenticated, loggin in"
+    message.style.opacity = 1;
     console.warn('signed in ')
-    signin(email)
     // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
     console.warn(errorCode)
+      var message = document.getElementById('logInMessage')
+    message.innerHTML = " an error ocurred,"+errorMessage;
+    message.style.opacity = 1;
+    message.style.backgroundColor = "#EF8787";
+    message.style.color = "#EEEEEE";
   });
-  writeUserData(javivijou2006, testemail)
+  //TODO redirection or action when logged in
+}
+function writeUserData() {
+  firebase.database().ref('users/test').set({
+    username: ' email',
+    email: '@.com',
+    profile_picture : 'image'
+  });
 }
